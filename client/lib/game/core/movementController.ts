edbbbef63476ff51843ee42e88a511e81d4c4637
Nate Schultz <nate.schultz@outlook.com>
2025-08-16 16:08:37 -0400

    fix camera height

--- client/lib/game/core/movementController.ts
+++ client/lib/game/core/movementController.ts
@@ -378,8 +378,8 @@ export class MovementController {
   }
 
   private updateCamera(camera: THREE.PerspectiveCamera, playerPos: THREE.Vector3): void {
-    const cameraHeight = 1.5;
-    const cameraDistance = 0.75;
+    const cameraHeight = 3;
+    const cameraDistance = 1.5;
     
     // Calculate camera position based on player's Y rotation
     const cameraX = playerPos.x + Math.sin(this.localPlayerRotation.y) * cameraDistance;
